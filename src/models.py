6e2551f88da039d963ad2fdf9b0f5ce4b39c7cdc
VR Yeshwanth <vryeshwanth06@gmail.com>
2026-08-17 19:24:57 +0530

    Experiment 1: Dropout Effect

--- src/models.py
+++ src/models.py
@@ -34,3 +34,50 @@ class BaselineCNN(nn.Module):
         x = self.conv_block(x)
 
         return self.classifier(x)
+
+class CNN(nn.Module):
+    def __init__(self, dropout=0.0, batch_norm=False):
+        super().__init__()
+
+        conv_layers = [
+            nn.Conv2d(
+                in_channels=1, out_channels=32,
+                kernel_size=3, stride=1, padding=1
+            )
+        ]
+
+        if batch_norm:
+            conv_layers.append(nn.BatchNorm2d(32))
+        conv_layers.append(nn.ReLU())
+
+        conv_layers.append(
+            nn.Conv2d(
+                in_channels=32, out_channels=64,
+                kernel_size=3, stride=1, padding=1
+            )
+        )
+        if batch_norm:
+            conv_layers.append(nn.BatchNorm2d(64))
+        conv_layers.extend([
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        ])
+
+        self.conv_block = nn.Sequential(*conv_layers)
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+
+            nn.Linear(64 * 14 * 14, 128),
+            nn.ReLU(),
+
+            nn.Dropout(dropout),
+
+            nn.Linear(128, 10)
+        )
+
+    def forward(self, x):
+        x = self.conv_block(x)
+        x = self.classifier(x)
+
+        return x
